0ba92b7085c9e1e3178027bbe533736e2e7b8c16
Bryan A <shodivine@gmail.com>
2012-11-14 09:09:24 -0600

    * spacing

--- wifite.py
+++ wifite.py
@@ -2023,10 +2023,10 @@ def has_handshake_tshark(target, capfile):
             while line.find('  ') != -1: line = line.replace('  ', ' ')
             
             fields = line.split(' ')
-			
-			# ensure tshark dumped correct info
-			if len(fields) < 5:
-				continue
+		
+            # ensure tshark dumped correct info
+            if len(fields) < 5:
+                continue
 
             src = fields[2].lower()
             dst = fields[4].lower()
